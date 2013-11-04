9c2ffc28c6a98131888b56fc8bec4f42c4c36a9e
olehshch <o.shcherbatyy@gmail.com>
2013-11-04 10:19:25 +0200

    FirstCommit

---
+++ src/main/java/com/lohika/seleniumtool/App.java
@@ -0,0 +1,13 @@
+package com.lohika.seleniumtool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/lohika/seleniumtool/Test1.java
@@ -0,0 +1,9 @@
+package com.lohika.seleniumtool;
+
+public class Test1 {
+
+	@org.testng.annotations.Test
+	public void Test() {
+		
+	}
+}
