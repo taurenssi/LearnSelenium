4e94d0f0de6f733ea56d45b0e2886a9e723e9f26
taurenssi <taurenssi@gmail.com>
2013-11-19 10:48:40 +0200

    Added 1 new test

---
+++ src/main/java/com/lohika/seleniumtool/SidebarTest.java
@@ -0,0 +1,72 @@
+package com.lohika.seleniumtool;
+
+import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import pages.LoginPage;
+import pages.MailPage;
+
+public class SidebarTest {
+private Logger logger = LoggerFactory.getLogger(LoginTest.class);
+	
+	@Test
+	public void OpenConversationFromSidebarTest() throws Exception{
+		
+		String searchName = "skypeqa011";
+		
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		WebDriver driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), capabilities);
+		
+		logger.info("Opening Outlook page");
+		driver.get("http://www.outlook.com/");
+		
+		LoginPage loginPage = new LoginPage();
+		PageFactory.initElements(driver, loginPage);
+		
+		logger.info("Loging in");
+		loginPage.username.sendKeys("svyatest9@hotmail.com");
+         
+        loginPage.password.sendKeys("skypet3st3r");
+         
+        loginPage.loginButton.click();
+         
+        MailPage mailPage = new MailPage();
+        PageFactory.initElements(driver, mailPage);
+        
+        logger.info("Opening sidebar");
+        mailPage.messagingIcon.click();
+        
+        logger.info("Check if sidebar is opened");
+        Assert.assertTrue(mailPage.sidebar.isDisplayed(), "Sidebar was not visible");
+        
+        logger.info("Looking for '" + searchName + "' contact");
+        mailPage.searchField.sendKeys(searchName);
+        
+        logger.info("Reinitializing elements on main page");
+        PageFactory.initElements(driver, mailPage);
+        
+        logger.info("Check if contact is found");
+        Assert.assertTrue(!mailPage.contactSearchInfoBar.isDisplayed(), "Contact 'Svyatoslav Saliy' wasn't found!");
+        
+        logger.info("Opening conversation with contact");
+        for (WebElement element: mailPage.contactSearchResult){
+        	if (element.getText().equals(searchName)){
+        		element.click();
+        	}
+        }
+        
+        logger.info("Check if conversation with " + searchName + " is opened");
+        Assert.assertTrue(mailPage.conversationTitle.getText().equals(searchName), "Conversation with contact 'Svyatoslav Saliy' wasn't opened!");
+            
+        driver.quit();
+	}
+}
--- src/main/java/pages/MailPage.java
+++ src/main/java/pages/MailPage.java
@@ -8,6 +8,29 @@ public class MailPage {
 	@FindBy (id = "contentFolderQuickViewList")
 	public WebElement contentFolder;
 	
+	@FindBy (id = "c_hiconm")
+	public WebElement messagingIcon;
+	
+	@FindBy (id = "sidebar")
+	public WebElement sidebar;
+	
+	@FindBy (id = "search_filter")
+	public WebElement searchField;
+	
+	@FindBy (xpath = "//*[@class='ContactSearchResult']")
+	public WebElement contactSearchResultsList;
+	
+	@FindBy (xpath = "//*[@class='t_mbgc t_amtc csIcWrap']")
+	public WebElement[] contactSearchResult;
+
+	@FindBy (xpath = "//*[@class='contactSearchInfoBar']")
+	public WebElement contactSearchInfoBar;
+	
+	@FindBy (xpath = "//*[@class='Title']")
+	public WebElement conversationTitle;
+
+	
+	
 	public boolean isDisplayed(){
 		return contentFolder.isDisplayed();
 	}
